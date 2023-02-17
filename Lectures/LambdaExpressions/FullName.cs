c5dc70cf71cccc3fb22011f5a7ad6d19e8d4e9ed
nposey1 <nposey1@ewu.edu>
2023-02-16 16:33:40 -0800

    Lecture notes

---
+++ Lectures/LambdaExpressions/FullName.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System.Reflection.Metadata.Ecma335;
+
+namespace LambdaExpressions;
+public record FullName(string FirstName, string LastName)
+{
+    public FullName(string fullName) : this(null!, null!)
+    {
+
+        if (fullName.Split(" ") is [string {Length: > 0 } firstName, string { Length: > 0 } lastName]) 
+        {
+            FirstName = firstName; 
+            LastName = lastName;
+        }
+    }
+
+    public string Initials 
+    {
+        get 
+        {
+            string initials;
+            //return $"{FirstName[0]}{LastName[0]}";
+            //return initials;
+            if ((FirstName.First(), LastName.First()) is (char firstInitial, char lastInitial))
+            {
+                initials = new string(new char[] { firstInitial, lastInitial });
+            }
+            return initials;
+        }
+    }
+
+}
---
+++ Lectures/LambdaExpressions/PatternMatchingTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LambdaExpressions;
+[TestClass]
+public class PatternMatchingTests
+{
+    [TestMethod]
+    public void SetName_IndigoMontoya_Success()
+    {
+        FullName testName = new FullName("Indigo Montoya");
+        (string firstName,string lastName) actual = (testName.FirstName, testName.LastName);
+        Assert.AreEqual<(string,string)>(("Indigo", "Montoya"), actual);
+        Assert.IsTrue(testName.FirstName is "Indigo");
+    }
+
+    [TestMethod]
+    public void Initials_InigoMontoya_Success()
+    {
+        FullName testName = new FullName("Inigo Montoya");
+        string actuals = testName.Initials;
+        Assert.AreEqual<string>("IM", actuals);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Create_EmptyStrings_Fails()
+    {
+        _ = new FullName("", "");
+    }
+
+    [TestMethod]
+    public void MyTestMethod()
+    {
+        (string, string) name = ("Inigo", "Montoya");
+        _ = name switch
+        {
+            (string {Length: > 0 and < 10 } firstName, string {Length: > 0 and < 10 } lastName) => true,
+
+        };
+    }
+}
